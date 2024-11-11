13f6212fd87d33d1a76aa6a503d66e205b0fb35d
TheBoardCoder <89057781+TheBoardCoder@users.noreply.github.com>
2024-11-11 11:09:18 -0500

    Update Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+/**
+ * The Policy class represents an insurance policy with various attributes 
+ * such as policy number, provider name, policyholder information, and 
+ * methods to calculate BMI and policy price.
+ */
 public class Policy {
    
    private String policyNumber;
@@ -19,6 +24,10 @@ public class Policy {
    private static final double BMI_MULTIPLIER = 703;
    private static final double SENIOR_STATUS = 50;
    
+   /**
+    * Default constructor that initializes a Policy object by prompting 
+    * the user for input values.
+    */
    public Policy() {
         Scanner scanner = new Scanner(System.in);
 
@@ -47,6 +56,18 @@ public class Policy {
         policyholderWeight = Double.parseDouble(scanner.nextLine());
     }
 
+    /**
+     * Constructs a Policy object with the specified values.
+     * 
+     * @param policyNumber the policy number
+     * @param providerName the provider name
+     * @param policyholderFirstName the policyholder's first name
+     * @param policyholderLastName the policyholder's last name
+     * @param policyholderAge the policyholder's age
+     * @param smokingStatus the policyholder's smoking status
+     * @param policyholderHeight the policyholder's height in inches
+     * @param policyholderWeight the policyholder's weight in pounds
+     */
     public Policy(String policyNumber, String providerName, String policyholderFirstName,
                   String policyholderLastName, int policyholderAge, String smokingStatus,
                   double policyholderHeight, double policyholderWeight) {
@@ -59,75 +80,165 @@ public class Policy {
         this.policyholderHeight = policyholderHeight;
         this.policyholderWeight = policyholderWeight;
     }
-
+   
+    /**
+     * Gets the policy number.
+     * 
+     * @return the policy number
+     */
     public String getPolicyNumber() {
         return policyNumber;
     }
-
+    
+    /**
+     * Sets the policy number.
+     * 
+     * @param policyNumber the new policy number
+     */
     public void setPolicyNumber(String policyNumber) {
         this.policyNumber = policyNumber;
     }
-
+   
+    /**
+     * Gets the provider name.
+     * 
+     * @return the provider name
+     */
     public String getProviderName() {
         return providerName;
     }
-
+   
+    /**
+     * Sets the provider name.
+     * 
+     * @param providerName the new provider name
+     */
     public void setProviderName(String providerName) {
         this.providerName = providerName;
     }
-
+   
+    /**
+     * Gets the policyholder's first name.
+     * 
+     * @return the policyholder's first name
+     */
     public String getPolicyholderFirstName() {
         return policyholderFirstName;
     }
-
+   
+    /**
+     * Sets the policyholder's first name.
+     * 
+     * @param policyholderFirstName the policyholder's new first name
+     */
     public void setPolicyholderFirstName(String policyholderFirstName) {
         this.policyholderFirstName = policyholderFirstName;
     }
-
+   
+    /**
+     * Gets the policyholder's last name.
+     * 
+     * @return the policyholder's last name
+     */
     public String getPolicyholderLastName() {
         return policyholderLastName;
     }
-
+   
+    /**
+     * Sets the policyholder's last name.
+     * 
+     * @param policyholderLastName the policyholder's new last name
+     */
     public void setPolicyholderLastName(String policyholderLastName) {
         this.policyholderLastName = policyholderLastName;
     }
-
+   
+    /**
+     * Gets the policyholder's age.
+     * 
+     * @return the policyholder's age
+     */
     public int getPolicyholderAge() {
         return policyholderAge;
     }
-
+   
+    /**
+     * Sets the policyholder's age.
+     * 
+     * @param policyholderAge the policyholder's new age
+     */
     public void setPolicyholderAge(int policyholderAge) {
         this.policyholderAge = policyholderAge;
     }
-
+   
+    /**
+     * Gets the policyholder's smoking status.
+     * 
+     * @return the policyholder's smoking status
+     */
     public String getSmokingStatus() {
         return policyholderSmokingStatus;
     }
-
+   
+    /**
+     * Sets the policyholder's smoking status.
+     * 
+     * @param policyholderSmokingStatus the new smoking status
+     */
     public void setSmokingStatus(String policyholderSmokingStatus) {
         this.policyholderSmokingStatus = policyholderSmokingStatus;
     }
-
+   
+    /**
+     * Gets the policyholder's height in inches.
+     * 
+     * @return the policyholder's height
+     */
     public double getPolicyholderHeight() {
         return policyholderHeight;
     }
-
+   
+    /**
+     * Sets the policyholder's height in inches.
+     * 
+     * @param policyholderHeight the policyholder's new height
+     */
     public void setPolicyholderHeight(double policyholderHeight) {
         this.policyholderHeight = policyholderHeight;
     }
-
+   
+    /**
+     * Gets the policyholder's weight in pounds.
+     * 
+     * @return the policyholder's weight
+     */
     public double getPolicyholderWeight() {
         return policyholderWeight;
     }
-
+   
+    /**
+     * Sets the policyholder's weight in pounds.
+     * 
+     * @param policyholderWeight the policyholder's new weight
+     */
     public void setPolicyholderWeight(double policyholderWeight) {
         this.policyholderWeight = policyholderWeight;
     }
-
+   
+    /**
+     * Calculates the Body Mass Index (BMI) of the policyholder.
+     * 
+     * @return the policyholder's BMI
+     */
     public double calculateBMI() {
         return (policyholderWeight * BMI_MULTIPLIER) / (policyholderHeight * policyholderHeight);
     }
-
+   
+    /**
+     * Calculates the price of the policy based on age, smoking status, and BMI.
+     * 
+     * @return the calculated policy price
+     */
     public double calculatePolicyPrice() {
         double price = BASE_FEE;
 
